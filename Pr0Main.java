7aa4077b5a8fc8d9a3f76f968d9dfd5fd76dd380
Julia Huergo Muñoz <95702056+juliahuergo@users.noreply.github.com>
2025-09-18 18:32:27 +0200

    Add files via upload

---
+++ Pr0Main.java
@@ -0,0 +1,10 @@
+package tp.pr0;
+
+public class Pr0Main {
+	public static void writeGreeting(String name) {
+		System.out.println("Hello, " + name);
+	}
+	public static void main(String args[]) {
+	      Pr0Main.writeGreeting("Bender Bending Rodríguez");
+	}
+}
